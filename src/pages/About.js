b26446254f33ea9aeb67c5142dee031ecf5e7284
Taaha-Aslam <www.taahaaslam16@gmail.com>
2024-08-11 16:49:29 +0500

    Portfolio

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AboutContainer = styled.div`
+  padding: 50px;
+  background-color: #f4f4f4;
+`;
+
+const About = () => {
+  return (
+    <AboutContainer>
+      <h2>About Me</h2>
+
+      <p>Hello! I'm [Taaha Aslam], a passionate MERN stack developer with the experience building dynamic,
+        responsive and user friendly web applications. My experties lies in leveraging the power of MongoDB,
+        Express.js, React.js and Node.js to create seamless and efficient solutions. </p>
+
+        <p>Experienced web developer adept in all stages of advanced web development.</p>
+
+        <p>Knowledgeable in user interface, testing and debugging processes.</p>
+
+        <p>Equipped with a diverse and promissing skill-set</p>
+
+        <p>Able to effectively self manage during independent projects, as well as collaborate in 
+            a team setting. </p>
+
+    <h2>Skills</h2>
+
+    <p></p>
+     
+    </AboutContainer>
+  );
+}
+
+export default About;
